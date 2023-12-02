7ab655f952e728ce3f379a33e347c7f4f0fab220
Jozelle <51460429+Jozelle@users.noreply.github.com>
2023-12-02 13:40:38 +0100

    Add files via upload

---
+++ Day2/Day2_Part2.cs
@@ -0,0 +1,60 @@
+﻿//Part 2
+StreamReader sr = new StreamReader("input.txt");
+
+int sum = 0;
+
+while (!sr.EndOfStream)
+{
+    int maxRed = 0;
+    int maxGreen = 0;
+    int maxBlue = 0;
+
+    string line = sr.ReadLine();
+
+    string[] firstSplit = line.Split(": ");
+
+    int gameNr = int.Parse(firstSplit[0].Split(' ')[1]);
+
+    string[] secondSplit = firstSplit[1].Split("; ");
+
+    foreach (string s in secondSplit)
+    {
+        string[] thirdSplit = s.Split(", ");
+
+        foreach (string s2 in thirdSplit)
+        {
+            if (s2.Contains("red"))
+            {
+                int sumRed = int.Parse(s2.Split(' ')[0]);
+
+                if (sumRed > maxRed)
+                {
+                    maxRed = sumRed;
+                }
+            }
+            else if (s2.Contains("green"))
+            {
+                int sumGreen = int.Parse(s2.Split(' ')[0]);
+
+                if (sumGreen > maxGreen)
+                {
+                    maxGreen = sumGreen;
+                }
+            }
+            else if (s2.Contains("blue"))
+            {
+                int sumBlue = int.Parse(s2.Split(' ')[0]);
+
+                if (sumBlue > maxBlue)
+                {
+                    maxBlue = sumBlue;
+                }
+            }
+        }
+    }
+
+    int total = maxRed * maxGreen * maxBlue;
+    sum += total;
+}
+
+Console.WriteLine(sum);
