7281935c59c79d94d511d2a62f684dbf6e83f796
Jozelle <51460429+Jozelle@users.noreply.github.com>
2023-12-04 22:40:03 +0100

    Add files via upload

---
+++ Day4/Day4_Part1.cs
@@ -0,0 +1,67 @@
+﻿string filename = "input.txt";
+StreamReader sr = new StreamReader(filename);
+
+double sum = 0;
+
+while (!sr.EndOfStream)
+{
+    string line = sr.ReadLine();
+    
+    int countMatchingNumbers = 0;   
+
+    string[] splitCard = SplitCard(line);
+    List<int> listWinningNumbers = SplitNumbers(splitCard[0]);
+    List<int> listMyNumbers = SplitNumbers(splitCard[1]);
+
+    foreach (int number in listWinningNumbers)
+    {
+        foreach (int number2 in listMyNumbers)
+        {
+            if (number == number2)
+            {
+                countMatchingNumbers++;
+            }
+        }
+    }
+
+    sum += CalculatePoints(countMatchingNumbers);
+}
+
+Console.WriteLine(sum);
+
+
+string[] SplitCard(string line)
+{
+    string[] removeCardTag = line.Split(":");
+
+    string[] dividedCard = removeCardTag[1].Split("|");
+    return dividedCard;
+}
+
+List<int> SplitNumbers(string line)
+{
+    string[] numbersTemp = line.Split(" ");
+    List<int> numbers = new List<int>();
+
+    foreach (string word in numbersTemp)
+    {
+        if (int.TryParse(word, out int number))
+        {
+            numbers.Add(number);
+        }
+       
+    }
+
+    return numbers;
+}
+
+double CalculatePoints(int matches)
+{
+    if (matches == 0) //Catch if there is no matching numbers
+    {
+        return 0;
+    }
+    
+    double points = Math.Pow(2, (matches - 1));
+    return points;
+}
---
+++ Day4/Day4_Part2.cs
@@ -0,0 +1,102 @@
+﻿List<Card> cards = new List<Card>();
+cards = FileToList();
+
+AddCopies();
+
+int totalCards = 0;
+foreach (Card card in cards)
+{
+    totalCards += card.copies;
+}
+
+Console.WriteLine(totalCards);
+
+
+List<Card> FileToList()
+    {
+    string filename = "input.txt";
+    StreamReader sr = new StreamReader(filename);
+
+    while (!sr.EndOfStream)
+    {
+        string line = sr.ReadLine();
+
+        string[] splitCard = SplitCard(line);
+        List<int> listWinningNumbers = SplitNumbers(splitCard[0]);
+        List<int> listMyNumbers = SplitNumbers(splitCard[1]);
+
+        int countMatchingNumbers = listWinningNumbers.Intersect(listMyNumbers).Count();
+
+        Card newCard = new Card(listWinningNumbers, listMyNumbers, countMatchingNumbers);
+
+        cards.Add(newCard);
+    }
+    sr.Close();
+
+    return cards;
+}
+
+string[] SplitCard(string line)
+{
+    string[] removeCardTag = line.Split(":");
+
+    string[] dividedCard = removeCardTag[1].Split("|");
+    return dividedCard;
+}
+
+List<int> SplitNumbers(string line)
+{
+    string[] numbersTemp = line.Split(" ");
+    List<int> numbers = new List<int>();
+
+    foreach (string word in numbersTemp)
+    {
+        if (int.TryParse(word, out int number))
+        {
+            numbers.Add(number);
+        }
+    }
+
+    return numbers;
+}
+
+double CalculatePoints(int matches)
+{
+    if (matches == 0) //Catch if there is no matching numbers
+    {
+        return 0;
+    }
+
+    double points = Math.Pow(2, (matches - 1));
+
+    return points;
+}
+
+void AddCopies()
+{
+    for (int i = 0; i < cards.Count; i++)
+    {
+        for (int j = i + 1; j <= i + cards[i].matches; j++)
+        {
+            cards[j].copies += cards[i].copies;
+        }
+    }
+}
+
+
+public class Card
+{
+    public int matches;
+    public int copies;
+    public List<int> winningNumbers;
+    public List<int> cardNumbers;
+
+    public Card(List<int> winningNumbers, List<int> cardNumbers, int matches)
+    {
+        this.copies = 1;
+        this.winningNumbers = winningNumbers;
+        this.cardNumbers = cardNumbers;
+        this.matches = matches;
+    }
+}
+
